46be102e20ecc9d2f8091b0da0e46db49b3c716d
lfbr0 <20361894+lfbr0@users.noreply.github.com>
2024-05-25 23:57:19 +0100

    no stop on line

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+    #app loop
+    while (True):
+        # Uncomment this block to pass the first stage
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    input()
+        # Wait for user input
+        input()
 
 
 if __name__ == "__main__":
