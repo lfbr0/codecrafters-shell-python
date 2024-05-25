4d6d957b324638b5c40f983674a8b7237dee2416
lfbr0 <20361894+lfbr0@users.noreply.github.com>
2024-05-26 00:18:02 +0100

    single file

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 import sys
-from command_handler import handle_input
 
-def main():
+"""
+Handle input from user
+"""
+def handle_input(input_args):
+    match input_args[0]:
+        
+            #handle exit from shell
+            case 'exit':
+                if len(input_args) < 2:
+                    exit(0)
+                else:
+                    try:
+                        exit_code = int(input_args[1])
+                        exit(exit_code)
+                    except ValueError:
+                        print(f"exit: return code {input_args[1]} is not an integer!")
+                    
+            #default case
+            case _:
+                print(f"{input_args[0]}: command not found")   
 
+"""
+Main function
+"""
+if __name__ == "__main__":
     try:
         #app loop
         while (True):
@@ -20,7 +42,3 @@ def main():
         pass
     except KeyboardInterrupt:
         pass #ignore, user wants to quit shell
-
-
-if __name__ == "__main__":
-    main()
