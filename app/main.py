3f6e88264285862730d1527b11397c8c1beab828
lfbr0 <20361894+lfbr0@users.noreply.github.com>
2024-05-26 00:36:33 +0100

    type command

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 import sys
+import shutil
+import os
+
+SHELL_BUILTINS = [
+    'echo',
+    'exit',
+    'type'
+]
 
 """
 Handle input from user
 """
 def handle_input(input_args):
     match input_args[0]:
-        
             #handle exit from shell
             case 'exit':
                 if len(input_args) < 2:
@@ -21,9 +28,30 @@ def handle_input(input_args):
             case 'echo':
                 print(' '.join(input_args[1:]))
             
+            #handle type
+            case 'type':
+                if len(input_args) < 2:
+                    print(f"type: not enough arguments passed!")
+                else:
+                    type_target = input_args[1]
+                    if type_target in SHELL_BUILTINS:
+                        print(f'{type_target} is a shell builtin')
+                    else:
+                        type_target_path = find_executable(type_target)
+                        if type_target_path == None:
+                            print(f"{type_target} not found")
+                        else:
+                            print(f"{type_target} is {type_target_path}")
+            
             #default case
             case _:
                 print(f"{input_args[0]}: command not found")   
+
+"""
+Find executable in PATH variable and return its path...
+"""
+def find_executable(target):
+    return shutil.which(target)
 
 """
 Main function
